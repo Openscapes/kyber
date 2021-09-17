a036d65fb69bd205a5c1a75e53438bc16d40e85d
seankross <smk240@gmail.com>
2021-09-16 18:25:33 -0700

    added tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(kyber)
+
+test_check("kyber")
---
+++ tests/testthat/test-ky_create_github_clinic.R
@@ -0,0 +1,10 @@
+test_that("The GitHub Clinic Folder can be created.", {
+  temp_dir <- tempdir()
+  clinic_path <- ky_create_github_clinic(path = temp_dir, names = c("julia", "erin"))
+  result <- all(c("erin.md", "julia.md") %in% list.files(clinic_path))
+  
+  # clean up
+  unlink(clinic_path, recursive = TRUE, force = TRUE)
+  
+  expect_true(result)
+})
