e1be8d8e66cd5ffe0e28a44a76b04eb09e0118ac
Andy Teucher <andy.teucher@gmail.com>
2024-05-29 12:15:06 -0700

    Document list_teams() and list_team_members()

--- R/add_team_members.R
+++ R/add_team_members.R
@@ -41,7 +41,23 @@ add_team_members <- function(team, members, org = "openscapes"){
   invisible(responses)
 }
 
-list_team_members <- function(team, org = "openscapes", names_only = TRUE) {
+#' List members of a GitHub Team
+#'
+#' @inheritParams add_team_members
+#' @param names_only Should only the team member names be returned (as a character vector; `TRUE`, the default),
+#'     or should all of the team member metadata be returned?
+#' @param ... passed on to [gh::gh()]
+#'
+#' @return a character vector of team member GitHub usernames if `names_only = TRUE`, otherwise
+#'    a `gh_response` object containing team member information
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   list_team_members(team = "2023-superdogs-test-cohort", org = "openscapes")
+#'   list_team_members(team = "2023-superdogs-test-cohort", org = "openscapes", names_only = FALSE)
+#' }
+list_team_members <- function(team, org = "openscapes", names_only = TRUE, ...) {
   check_gh_pat()
 
   org_teams <- list_teams(org)
@@ -54,7 +70,8 @@ list_team_members <- function(team, org = "openscapes", names_only = TRUE) {
   team_members <- gh(
     "GET /orgs/{org}/teams/{team_slug}/members",
     org = org,
-    team_slug = team
+    team_slug = team,
+    ...
   )
 
   if (!names_only) return(team_members)
@@ -62,7 +79,22 @@ list_team_members <- function(team, org = "openscapes", names_only = TRUE) {
   vapply(team_members, `[[`, FUN.VALUE = character(1), "login")
 }
   
-list_teams <- function(org = "openscapes", names_only = TRUE) {
+#' List teams in a GitHub organization
+#'
+#' @inheritParams list_team_members
+#' @param names_only Should only the team names be returned (as a character vector; `TRUE`, the default),
+#'     or should all of the team metadata be returned?
+#'
+#' @return a character vector of team names if `names_only = TRUE`, otherwise
+#'    a `gh_response` object containing team information
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   list_teams(org = "openscapes")
+#'   list_teams(org = "openscapes", names_only = FALSE)
+#' }
+list_teams <- function(org = "openscapes", names_only = TRUE, ...) {
   check_gh_pat()
     
   teams <- gh("GET /orgs/{org}/teams", org = org)
