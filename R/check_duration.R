9a21c0cf8d95ba81164d6bed6b08aafd7ac0d3a3
seankross <smk240@gmail.com>
2024-09-23 02:14:09 -0700

    added check_duration

---
+++ R/check_duration.R
@@ -0,0 +1,32 @@
+#' Check the duration of call
+#' 
+#' @inheritParams call_agenda
+#' @importFrom cli cli_bullets
+check_duration <- function(registry_url, cohort_id, call_number,
+                           cohort_sheet = "cohort_metadata", 
+                           call_sheet = "call_metadata"){
+  cohort_registry <- read_sheet(registry_url, cohort_sheet, col_types = "c")
+  call_registry <- read_sheet(registry_url, call_sheet, col_types = "c")
+  
+  cohort_type_ <- get_cohort_type(cohort_registry, cohort_id)
+  template_files <- get_template_files(call_registry, call_number, cohort_type_)
+  
+  warn_if_any_not_rmd(template_files)
+  stop_if_any_templates_not_installed(template_files)
+  
+  template_files <- template_files %>% 
+    map_chr(kyber_file)
+  
+  template_params <- template_files %>% 
+    map(yaml_front_matter) %>% 
+    map(~.x$params)
+  
+  durations <- template_params %>% 
+    map_dbl(~ ifelse(is.null(.x$duration), NA, .x$duration))
+  
+  total_duration <- template_params %>% 
+    map_dbl(~ ifelse(is.null(.x$total_duration), NA, .x$total_duration))
+    
+  
+  })
+}
