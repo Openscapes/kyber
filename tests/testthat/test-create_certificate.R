89286c9ce69df7d9ff45b4e02a0dfbe7395df566
Andy Teucher <andy.teucher@gmail.com>
2024-12-09 17:36:30 -0800

    Add a test

--- tests/testthat/test-create_certificate.R
+++ tests/testthat/test-create_certificate.R
@@ -15,3 +15,22 @@ test_that("create_certificate works", {
     file.path(tdir, "OpenscapesCertificate_Test-cohort_Jane-Doe.pdf")
   ))
 })
+
+test_that("create_certificate works with nmfs", {
+  tdir <- withr::local_tempdir()
+  
+  cert_path <- create_certificate(
+    "Test cohort",
+    first_name = "Jane",
+    last_name = "Doe",
+    start_date = "Oct 19",
+    end_date = "Dec 19",
+    cohort_website = "https://google.com",
+    cohort_type = "nmfs",
+    output_dir = tdir
+  )
+  
+  expect_true(file.exists(
+    file.path(tdir, "nmfsOpenscapesCertificate_Test-cohort_Jane-Doe.pdf")
+  ))
+})
